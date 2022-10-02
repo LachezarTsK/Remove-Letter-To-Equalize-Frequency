8039df6c778cafb389b8ba000f2ba6dcc3e5b295
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-10-02 09:16:51 +0200

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -5,7 +5,7 @@
  */
 var equalFrequency = function (word) {
 
-    this.ALPHABET_SIZE = 26;
+    const ALPHABET_SIZE = 26;
     const asciiCodeFor_a = 97;
     const frequency = new Array(ALPHABET_SIZE).fill(0);
 
@@ -33,15 +33,15 @@ var equalFrequency = function (word) {
  */
 function allFrequenciesAreEqual(frequency) {
     let firstNonzeroFrequency = 0;
-    for (let i = 0; i < this.ALPHABET_SIZE; ++i) {
-        if (frequency[i] === 0) {
+    for (let currentFrequency of frequency) {
+        if (currentFrequency === 0) {
             continue;
         }
         if (firstNonzeroFrequency === 0) {
-            firstNonzeroFrequency = frequency[i];
+            firstNonzeroFrequency = currentFrequency;
             continue;
         }
-        if (firstNonzeroFrequency !== frequency[i]) {
+        if (firstNonzeroFrequency !== currentFrequency) {
             return false;
         }
     }
